9681efc66030303303f1c4b9e24067ea8b4c6e8f
orphee-celui-qui-ne-sait-rien <79347623+orphee-celui-qui-ne-sait-rien@users.noreply.github.com>
2021-12-27 21:48:53 -0600

    Update Chapter2.hs

--- Chapter2.hs
+++ Chapter2.hs
@@ -1,3 +1,10 @@
 sayHello :: String -> IO ()
 sayHello x = 
     putStrLn ("Hello, " ++ x ++ "!")
+
+-- Exercises: Comprehension check
+half x = x/2
+square x = x**2
+
+piAndSquareProd :: Floating a => a -> a
+piAndSquareProd n = pi * (n**2)
