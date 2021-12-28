7a041ccbdf4f1d42537e0ab986b6eff619876c90
orphee-celui-qui-ne-sait-rien <79347623+orphee-celui-qui-ne-sait-rien@users.noreply.github.com>
2021-12-28 00:56:56 -0600

    updates

--- Chapter2/chapter2.hs
+++ Chapter2/chapter2.hs
@@ -7,3 +7,22 @@ half x = x/2
 square x = x**2
 
 piAndSquareProd n = pi * (n**2)
+
+-- Exercises: A Head Code
+var1 = x
+     where x = 5
+
+var2 = x
+     where x = 5**2
+
+var3 = x * y
+     where x = 5
+           y = 6
+var4 = x + 3
+     where x = 3
+           y = 1000
+
+var5 = z/x + y 
+     where x = 7
+           y = negate x
+           z = y * 10
