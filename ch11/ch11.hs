e117bbadabe98ecc83366a337aad63f7591c12c6
orphee-celui-qui-ne-sait-rien <79347623+orphee-celui-qui-ne-sait-rien@users.noreply.github.com>
2022-04-22 19:08:02 -0500

    ch11 progress

---
+++ ch11/ch11.hs
@@ -0,0 +1,13 @@
+-- Algebraic datatypes
+-- newtype
+-- {-# LANGUAGE GeneralizedNewtypeDeriving #-} to derive all typeclasses
+newtype Goats = Goats Int deriving (Eq, Show)
+
+class TooMany a where
+    tooMany :: a -> Bool
+
+instance TooMany Int where
+    tooMany n = n > 42
+
+instance TooMany Goats where
+    tooMany (Goats n) = n >43
