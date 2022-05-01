e3403e98707d86801b9544b75669bab1fca0cc88
orphee-celui-qui-ne-sait-rien <79347623+orphee-celui-qui-ne-sait-rien@users.noreply.github.com>
2022-04-30 19:29:41 -0500

    ch11 progress

--- ch11/ch11.hs
+++ ch11/ch11.hs
@@ -12,3 +12,63 @@ instance TooMany Int where
 instance TooMany Goats where
     tooMany (Goats n) = n >43
 
+
+
+data OperatingSystem =
+       GnuPlusLinux
+    | OpenBSDPlusNevermindJustBSDStill | Mac
+    | Windows
+    deriving (Eq, Show)
+data ProgLang =
+       Haskell
+    | Agda
+    | Idris
+    | PureScript deriving (Eq, Show)
+data Programmer =
+    Programmer { os :: OperatingSystem
+                , lang :: ProgLang }
+                deriving (Eq, Show)
+
+allOperatingSystems :: [OperatingSystem]
+allOperatingSystems = [ GnuPlusLinux,
+                        OpenBSDPlusNevermindJustBSDStill,
+                        Mac,
+                        Windows ]
+
+allLanguages :: [ProgLang]
+allLanguages = [Haskell, Agda, Idris, PureScript]
+
+allProgrammers :: [Programmer]
+allProgrammers = [Programmer x y | x <- allOperatingSystems, y <- allLanguages]
+
+
+newtype Name = Name String deriving (Show, Eq)
+newtype Acres   = Acres Int deriving (Show, Eq)
+-- FarmerType is a Sum
+data FarmerType = DairyFarmer
+                | WheatFarmer
+                | SoybeanFarmer
+                deriving (Show, Eq)
+-- Farmer is a plain ole product of -- Name, Acres, and FarmerType
+data Farmer =
+  Farmer Name Acres FarmerType
+  deriving (Show, Eq)
+
+isSthFarmer :: FarmerType -> Farmer -> Bool
+isSthFarmer toMatch (Farmer _ _ typeFarmer) = typeFarmer == toMatch
+
+isDairyFarmer :: Farmer -> Bool
+isDairyFarmer = isSthFarmer DairyFarmer
+
+bookIsDairyFarmer :: Farmer -> Bool
+bookIsDairyFarmer (Farmer _ _ DairyFarmer) = True
+bookIsDairyFarmer _                        = False 
+
+data FarmerRec = FarmerRec {name :: Name,
+                            acres :: Acres,
+                            farmerType :: FarmerType}
+
+isDairyFarmerRec farmer = case farmerType farmer of
+                          DairyFarmer -> True 
+                          _           -> False
+
