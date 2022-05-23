d7f62a9905c9a86af49f2a271c35853957a97862
orphee-celui-qui-ne-sait-rien <79347623+orphee-celui-qui-ne-sait-rien@users.noreply.github.com>
2022-05-22 23:17:15 -0500

    ch12 progress

--- ch12/ch12.hs
+++ ch12/ch12.hs
@@ -1,4 +1,6 @@
-import Data.Char (isLetter)
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use if" #-}
+import Data.Char (isLetter, isAlpha)
 -- Chapper 12: Signaling adversity
 
 -- 12.2
@@ -53,16 +55,165 @@ replaceThe str = unwords (map replaceThe wrds)
           replaceThe x = if isThe x
                          then "a"
                          else x
-isThe = (==) "the"
 
--- a mess, doesnt compile
+isThe :: String -> Bool
+isThe [] = False
+isThe x = (==) "the" x
+
+startsWithVow :: String -> Bool
+startsWithVow [] = False
+startsWithVow (x:_)
+    | x `elem` "aeiou" = True
+    | otherwise = False
+
+
+
 countTheBeforeVowel :: String -> Integer
 countTheBeforeVowel [] = 0
-countTheBeforeVowel str 
-    | isThe x && isNextVow y = 1 + countTheBeforeVowel remainder
-    | otherwise = 0 + countTheBeforeVowel remainder
-    where wrds = words str
-          (x:y) = take 2 wrds
-          isNextVow [] = False
-          isNextVow (z:_) = isLetter z
-          remainder = concat (drop 2 wrds)
+countTheBeforeVowel str = helper $ words str
+    where helper :: [String] -> Integer
+          helper [] = 0
+          helper (x:xs) = case isThe x of
+             False -> helper xs
+             True -> case xs of
+                 [] -> 0
+                 _  -> case startsWithVow (head xs) of
+                        False -> helper xs
+                        True -> 1 + helper xs
+
+-- Validate the word
+newtype Word' = Word' String deriving (Eq, Show)
+type NVows = Int
+type NCons = Int
+
+vowels :: [Char]
+vowels ="aeiou"
+
+consonants :: [Char]
+consonants = "bcdfghjklmnpqrstvwxyz"
+
+mkWord :: String -> Maybe Word'
+mkWord [] = Nothing
+mkWord str = case vows < cons of
+    False -> Nothing
+    True -> Just (Word' str)
+
+    where helper :: String -> (NCons,NVows) -> (NCons,NVows)
+          helper [] tup = tup
+          helper (x:xs) tup@(vows,cons)
+            | x `elem` consonants = helper xs (vows,cons+1)
+            | x `elem` vowels     = helper xs (vows+1,cons)
+            | otherwise           = helper xs tup
+          (vows,cons) = helper str (0,0)
+
+-- It's only Natural
+data Nat = Zero | Succ Nat deriving (Eq, Show)
+
+natToInteger :: Nat -> Integer
+natToInteger Zero = 0
+natToInteger (Succ x) = 1 + natToInteger x
+
+integerToNat :: Integer -> Maybe Nat
+integerToNat x
+    | 0 > x = Nothing
+    | otherwise = Just (helper x)
+    where helper 0 = Zero
+          helper y = Succ (helper (y-1))
+
+-- Small library for Maybe
+-- 1. Simple boolean checks for Maybe values
+isJust :: Maybe a -> Bool
+isJust Nothing = False
+isJust _ = True
+
+isNothing :: Maybe a -> Bool
+isNothing x = not $ isJust x
+
+-- 2. Maybe catamorphism
+mayybee :: b -> (a -> b) -> Maybe a -> b
+mayybee x _ Nothing = x
+mayybee _ f (Just y) = f y
+
+-- 3. In case you want to provide a fallback value
+fromMaybe :: a -> Maybe a -> a
+fromMaybe x Nothing = x
+fromMaybe _ (Just x) = x
+
+fromMaybe2 :: a -> Maybe a -> a
+fromMaybe2 x = mayybee x id
+
+-- 4. Converting between List and Maybe
+listToMaybe :: [a] -> Maybe a
+listToMaybe [] = Nothing
+listToMaybe (x:_) = Just x
+
+maybeToList :: Maybe a -> [a]
+maybeToList Nothing = []
+maybeToList (Just a) = [a]
+
+-- 5. For when we want to drop the Nothing values from our list
+catMaybes :: [Maybe a] -> [a]
+catMaybes [] = []
+catMaybes (x:xs) = case x of
+    Nothing -> catMaybes xs
+    Just x -> x : catMaybes xs
+
+-- 6. You'll see this called "sequence" later
+flipMaybe :: [Maybe a] -> Maybe [a]
+flipMaybe maybes = helper maybes []
+    where helper :: [Maybe a] -> [a] -> Maybe [a]
+          helper [] carry = Just carry
+          helper (Nothing:_) _ = Nothing
+          helper (Just x:xs) carry = helper xs (carry++[x])
+
+-- Small library for Either 
+-- 1. Try to eventually arrive at a solution that uses foldr, 
+-- even if earlier versions don’t use foldr.
+lefts'1 :: [Either a b] -> [a]
+lefts'1 [] = []
+lefts'1 (Left a:xs) = a:lefts'1 xs
+lefts'1 (Right _:xs) = lefts'1 xs
+
+lefts' :: [Either a b] -> [a]
+lefts' = foldr helper []
+    where helper :: Either a b -> [a] -> [a]
+          helper (Left a) next  = [a] ++ next -- without (++) it wont work
+          helper (Right _) next = next
+{- 
+foldr helper [] [Left 1, Right 'a', Left 2]
+Left 1 `helper` (foldr helper [] [Right 'a',Left 2])
+Left 1 `helper` (Right 'a' `helper`(foldr helper [] [Left 2]))
+Left 1 `helper` (Right 'a' `helper`(Left 2 `helper` []))
+ -}
+
+ -- 2. Same as the last one. Use foldr eventually
+rights' :: [Either a b] -> [b]
+rights' = foldr helper []
+    where helper :: Either a b -> [b] -> [b]
+          helper (Right b) next = [b] ++ next -- without (++) it wont work
+          helper (Left _)  next = next
+
+-- 3. partitionEithers'
+partitionEithers' :: [Either a b] -> ([a],[b])
+partitionEithers' eithers = helper eithers ([],[])
+    where helper :: [Either a b] -> ([a],[b]) -> ([a],[b])
+          helper [] carry = carry
+          helper (Left a:xs) (lefs,righs) = helper xs (lefs++[a],righs)
+          helper (Right a:xs) (lefs,righs) = helper xs (lefs,righs++[a])
+
+-- 4. eitherMaybe'
+eitherMaybe' :: (b -> c) -> Either a b -> Maybe c
+eitherMaybe' _ (Left _) = Nothing
+eitherMaybe' f (Right a) = Just (f a)
+
+-- 5. This is a general catamorphism for Either values
+either' :: (a -> c) -> (b -> c) -> Either a b -> c
+either' f _ (Left a) = f a
+either' _ f (Right b) = f b
+
+-- 6. Same as before, but use the either' function you just wrote
+eitherMaybe'' :: (b -> c) -> Either a b -> Maybe c
+eitherMaybe'' _ (Left _) = Nothing
+eitherMaybe'' f (Right b) = Just (f b)
+
+-- Unfolds 
