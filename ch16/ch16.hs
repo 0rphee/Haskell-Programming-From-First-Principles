c6de2acd720c935d67397f883236bef6477e2d51
orphee-celui-qui-ne-sait-rien <79347623+orphee-celui-qui-ne-sait-rien@users.noreply.github.com>
2022-06-05 15:03:30 -0500

    ch16 progress

--- ch16/ch16.hs
+++ ch16/ch16.hs
@@ -1,1 +1,7 @@
 -- Chapter 16: Functor
+
+data BTree a = Leaf | Node (BTree a) a (BTree a)
+  deriving (Show,Eq)
+instance Functor BTree where
+  fmap _ Leaf = Leaf
+  fmap f (Node l val r) = Node (fmap f l) (f val) (fmap f r)
