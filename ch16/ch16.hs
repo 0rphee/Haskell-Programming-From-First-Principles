f618f1a2fd857792ae552f93edee6abebaa06abd
orphee-celui-qui-ne-sait-rien <79347623+orphee-celui-qui-ne-sait-rien@users.noreply.github.com>
2022-06-18 20:24:10 -0500

    ch16 progress

--- ch16/ch16.hs
+++ ch16/ch16.hs
@@ -5,3 +5,14 @@ data BTree a = Leaf | Node (BTree a) a (BTree a)
 instance Functor BTree where
   fmap _ Leaf = Leaf
   fmap f (Node l val r) = Node (fmap f l) (f val) (fmap f r)
+
+
+-- 16.6 The Good the Bad, and the Ugly
+data WhoCares a = ItDoesnt | Matter a | WhatThisIsCalled
+                  deriving (Eq, Show)
+
+instance Functor WhoCares where
+  fmap _ ItDoesnt = ItDoesnt
+  fmap _ WhatThisIsCalled = WhatThisIsCalled
+  fmap f (Matter a) = Matter (f a)
+
