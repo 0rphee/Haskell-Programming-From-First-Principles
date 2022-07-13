c5ec1301b78dba978fbc367c1bb4620494dc549e
0rphee <79347623+0rphee@users.noreply.github.com>
2022-07-12 19:00:45 -0500

    add ch23

---
+++ ch23/state-test/src/RandomExample2.hs
@@ -0,0 +1,26 @@
+module RandomExample2 where
+
+import Control.Applicative (liftA3)
+import Control.Monad (replicateM)
+import Control.Monad.Trans.State
+import System.Random
+import RandomExample
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
